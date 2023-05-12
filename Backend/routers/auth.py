cbbc38b6b3e322aad7defdb7b7df53930344e9e9
galileo-dev <fionn2200@gmail.com>
2023-05-12 18:35:55 +0100

    reformat

--- Backend/routers/auth.py
+++ Backend/routers/auth.py
@@ -8,31 +8,32 @@ import os
 router = APIRouter()
 
 
-
-
-
 # signup endpoint (not implemented)
 
+
 # login endpoint
-async def get_user_token(res: Response, credential: HTTPAuthorizationCredentials = Depends(HTTPBearer(auto_error=False))):
+async def get_user_token(
+    res: Response,
+    credential: HTTPAuthorizationCredentials = Depends(HTTPBearer(auto_error=False)),
+):
     if credential is None:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Bearer authentication is needed",
-            headers={'WWW-Authenticate': 'Bearer realm="auth_required"'},
+            headers={"WWW-Authenticate": 'Bearer realm="auth_required"'},
         )
     try:
         print(credential.credentials)
-        
+
         decoded_token = auth.verify_id_token(credential.credentials)
     except Exception as err:
         traceback.print_exc()
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail=f"Invalid authentication from Firebase. {err}",
-            headers={'WWW-Authenticate': 'Bearer error="invalid_token"'},
+            headers={"WWW-Authenticate": 'Bearer error="invalid_token"'},
         )
-    res.headers['WWW-Authenticate'] = 'Bearer realm="auth_required"'
+    res.headers["WWW-Authenticate"] = 'Bearer realm="auth_required"'
     return decoded_token
 
 
@@ -43,4 +44,4 @@ async def hello():
 
 @router.get("/api/user_token")
 async def hello_user(user=Depends(get_user_token)):
-    return {"msg": "Hello, user", "uid": user['uid']}
+    return {"msg": "Hello, user", "uid": user["uid"]}
